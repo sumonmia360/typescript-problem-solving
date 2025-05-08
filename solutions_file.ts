44e57f7d9b88a0fd0574ed080940483beec773e8
sumonmia360 <sumonmia4526@gmail.com>
2025-05-08 12:54:22 +0600

    From the parent classes to child classes extends and use case

--- solutions_file.ts
+++ solutions_file.ts
@@ -24,5 +24,37 @@
   }
 
   const processValueCheck = processValue(10);
-  console.log(processValueCheck);
+  //   console.log(processValueCheck);
+
+  //==
+  class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make: string, year: number) {
+      (this.make = make), (this.year = year);
+    }
+
+    getInfo() {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
+
+  class Car extends Vehicle {
+    private model: string;
+    constructor(make: string, year: number, model: string) {
+      super(make, year);
+      this.model = model;
+    }
+    getModel() {
+      return `Model: ${this.model}`;
+    }
+  }
+
+  const myCar = new Car("Toyota", 2020, "Corolla");
+  console.log(myCar.getInfo());
+  console.log(myCar.getModel());
+
+  //==
+
+  ///
 }
